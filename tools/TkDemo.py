928d3f897cf637a3911b41637c6a77a76fe972c5
Cedric Adjih <cedric.adjih@inria.fr>
2015-06-18 04:54:17 -0700

    final demo watr.li

--- tools/TkDemo.py
+++ tools/TkDemo.py
@@ -4,13 +4,30 @@
 import Tkinter, os
 from Tkinter import *
 
-import optparse
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--socket-id", type=int, default=0) # XXX:useless
+parser.add_argument("--name", type=str, default=None)   # XXX:useless
+args = parser.parse_args()
+
+if args.name == None:
+    socketId = args.socket_id
+    moteSelect = "--socket-id %s" % args.socket_id
+    moteId = "@%s" % args.socket_id
+else:
+    moteSelect = "--name %s" % args.name
+    moteId = args.name
 
 
-parser = optparse.OptionParser()
-parser.add_option("--mote-id", dest="moteId", action="store", default=0,
-     type="int")
-(options, argList) = parser.parse_args()
+#import optparse
+#
+#parser = optparse.OptionParser()
+#parser.add_option("--mote-id", dest="moteId", action="store", default=0,
+#     type="int")
+#(options, argList) = parser.parse_args()
+
 
 CommandList = [
     ("ping-rpi",
@@ -20,19 +37,26 @@ CommandList = [
      """roxterm -T sniffer -e bash -c './watrli-tool sniffer ; echo DONE ;sleep 10'&"""),
     ("wireshark", 'sudo wireshark -k -i lo -Y "zep" &'),
 
-    ( "", "echo ok"),
+    ( "RPi Process", None),
 
-    ( "rpi-setup", "./watrli-tool rpi-setup"),
-    ( "rpi-dashboard", "./watrli-tool rpi-dashboard"),
-    ( "rpi-proxy", "./watrli-tool rpi-proxy"),
-    ( "rpi-rpld", "./watrli-tool rpi-rpld"),
+    ( "rpi-setup", "./watrli-tool rpi-setup" ),
+    ( "rpi-clean-lock", "./watrli-tool rpi-clean-lock" ),
+    ( "rpi-dashboard", "./watrli-tool rpi-dashboard" ),
+    ( "rpi-proxy", "./watrli-tool rpi-proxy" ),
+    ( "rpi-rpld", "./watrli-tool rpi-rpld" ),
 
-    ( "", "echo ok"),
+    ( "SamR21 XPro", None),
 
     ( "stop-r21", "./watrli-tool dbg-server"),
     ( "flash-r21", "./watrli-tool flash"),
     ( "reset-r21", "./watrli-tool reset"),
     ( "term-r21", "./watrli-tool term"),
+
+    ( "M3", None ),
+
+    ( "flash-m3", "./watrli-tool flash iot-lab_M3"),
+    ( "reset-m3", "./watrli-tool reset iot-lab_M3"),
+    ( "term-m3", "./watrli-tool term iot-lab_M3")
 
     # ("reuse-port",
     #  """roxterm -T 'cmd' -e bash -c 'sudo bash -c "echo 1 > /proc/sys/net/ipv4/tcp_fin_timeout"; sudo bash -c "echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle" ; sudo bash -c "echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse" ; echo DONE ;sleep 10' &"""),
@@ -84,42 +108,64 @@ CommandList = [
     #("shake", """roxterm --fork -T shake -e bash -c 'W=$(wmctrl -l | grep -i foren6 | cut "-d " -f1); echo "moving window $W (foren6) constantly" ; D=0.1 ; U=350; V=351 ; while true ; do wmctrl -i -r $W -e 0,-1,$U,-1,-1 ; wmctrl -i -r $W -e 0,-1,$V,-1,-1 ; sleep $D ; done'"""),
 
     #("!smartrf-sniffer", "./expctl foren6-sniffers --output wireshark+smartrf"),
-    ("!wireshark-zep", 'sudo wireshark -k -i lo -Y "zep" &')
+    #("!wireshark-zep", 'sudo wireshark -k -i lo -Y "zep" &')
 ]
 
-if "only-control" in argList:
-    CommandList += ExtraCommandList
 
 class Application(Frame):
 
     def create(self):
-        self.buttonQuit = Button(self, text="QUIT", fg="red", command=self.quit)
-        self.buttonQuit.pack({"side": "left"})
+        self.controlFrame = Frame(self)
+        self.controlFrame.pack({"side": "left"})
+        self.buttonQuit = Button(self.controlFrame, text="QUIT", fg="red", 
+                                 command=self.quit)
+        self.buttonQuit.pack({"side": "top", "fill": "x"})
+        self.buttonQuit = Button(self.controlFrame, text="restart", #fg="red", 
+                                 command=self.restart)
+        self.buttonQuit.pack({"side": "left", "fill": "x"})
+
         
+        currentFrame = self
         self.buttonList = []
         for i,(name, command) in enumerate(CommandList):
-            def _runCommand(command=command):
-                self.runCommand(command)
-            button = Button(self, text=name, command=_runCommand)
-            #if i % 5 == 0:
-            #    button.pack({"side":"left"})
-            button.pack({"side":"top"})
-            self.buttonList.append(button)
+            if command != None:
+                def _runCommand(command=command):
+                    self.runCommand(command)
+                button = Button(currentFrame, text=name, command=_runCommand)
+                #if i % 5 == 0:
+                #    button.pack({"side":"left"})
+                button.pack({"side":"top", "fill": "x"})
+                self.buttonList.append(button)
+            else:
+                #currentFrame = Frame(self, bd=3, relief=RIDGE)
+                #currentFrame.pack({"side":"top", "fill":"x"})
+                #label = Label(currentFrame, text=name)
+                #label.pack({"side":"top", "fill": "x"})
+                currentFrame = LabelFrame(self, text = name)
+                currentFrame.pack({"side":"top", "fill":"x"})
+
+    def restart(self):
+        argList = ["python", "python"] + sys.argv
+        os.execlp(*argList)
 
     def runCommand(self, command):
-        command = command.replace("./hctl", "./hctl --mote-id %s" 
-                                  % options.moteId)
         print "+", command
         os.system(command)
 
-    def __init__(self, options, master=None):
-        self.options = options
+    def __init__(self, args, master=None):
+        self.args = args
         Frame.__init__(self, master)
         self.pack()
         self.create()
 
 root = Tk()
-app = Application(options, master=root)
+bgColor = "#d1e3f2"
+activeBgColor = "#428bca"
+root.wm_title("Exp. Tools")
+root.tk_setPalette(background=bgColor, foreground='black',
+                   activeBackground=activeBgColor, 
+                   activeForeground=bgColor)
+app = Application(args, master=root)
 app.mainloop()
 root.destroy()
 
